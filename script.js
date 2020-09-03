e3b6a732237959d8d89ca7c6cfbf3d164e41307a
AmaraCP <preshchilaka06@gmail.com>
2020-09-03 10:30:58 +0100

    working on the rooms section

--- script.js
+++ script.js
@@ -15,3 +15,43 @@ window.addEventListener('scroll', function() {
     document.querySelector('.navbar').style.backgroundColor = "transparent";
   } 
 });
+
+
+$(document).ready(function(){
+  $('.room-wrap').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          // mobileFirst: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          // infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          // mobileFirst: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          // mobileFirst: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
